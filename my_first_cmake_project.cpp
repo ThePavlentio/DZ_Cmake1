7bf625be4fd06b165d56fe846481c9c4e425d463
Pavel Nikolaev <altertumlicher@gmail.com>
2025-04-24 10:20:04 +0500

    initial commit

---
+++ my_first_cmake_project.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::string username{};
+
+	std::cout << "Введите имя: ";
+	std::getline(std::cin >> std::ws, username);
+	std::cout << "Здравствуйте, " << username << '!' << std::endl;
+
+	return 0;
+}
